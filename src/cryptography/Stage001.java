917b15098a4d890e2cc2d48ce99cd89e27fd667a
euclides <euclides.rodriguesjr@outlook.com>
2020-03-18 10:29:10 -0300

    Stage001

--- src/cryptography/Stage001.java
+++ src/cryptography/Stage001.java
@@ -20,26 +20,26 @@ public class Stage001 {
 		for (int i = 0; i < mens.length(); i++) {
 			// Tratamento Letras minusculas
 			if (mens.charAt(i) >= 97 && mens.charAt(i) <= 122) {// letras minusculas de acordo com a tabela ASCII
-				if ((int) (mens.charAt(i) + chave) > 122) {
-					x = (char) (mens.charAt(i) + chave);
+				if ((int) (mens.charAt(i) - chave) > 122) {
+					x = (char) (mens.charAt(i) - chave);
 					y = (char) (x - 122);
 					ascii = (char) (96 + y);
 					System.out.print(ascii + " ");
 				} else {
-					ascii = (char) (mens.charAt(i) + chave);
+					ascii = (char) (mens.charAt(i) - chave);
 					System.out.print(ascii + " ");
 
 				}
 			}
 			// Tratamento Letras mausculas
 			if (mens.charAt(i) >= 65 && mens.charAt(i) <= 90) {
-				if (mens.charAt(i) + chave > 90) {
-					x = (char) (mens.charAt(i) + chave);
+				if (mens.charAt(i) - chave > 90) {
+					x = (char) (mens.charAt(i) - chave);
 					y = (char) (x - 90);
 					ascii = (char) (64 + y);
 					System.out.print(ascii + " ");
 				} else {
-					ascii = (char) (mens.charAt(i) + chave);
+					ascii = (char) (mens.charAt(i) - chave);
 					System.out.print(ascii + " ");
 				}
 			}
